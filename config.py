590320e3f379416162dc31674da2f77fd2468b06
Bwunz <redlinejo@gmail.com>
2025-12-06 12:14:57 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cefwqdfigulk.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "joshualoo-employee"
+customregion = "us-east-1"
+
