195b39c2843a5f6477c9e87efecf4e15141f1177
Bwunz <redlinejo@gmail.com>
2025-12-06 12:27:35 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 customhost = "employee.cefwqdfigulk.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
-customdb = "employee"
+customdb = "employee_db"
 custombucket = "joshualoo-employee"
 customregion = "us-east-1"
 
